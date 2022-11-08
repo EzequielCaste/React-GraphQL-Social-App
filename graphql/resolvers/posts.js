5bb622c2815d08173222d5a056e811ad82a4d511
Ezequiel Castellanos <51804994+EzequielCaste@users.noreply.github.com>
2022-11-08 16:38:01 -0300

    add comments to post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -29,6 +29,10 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
+      if(args.body.trim() === '') {
+        throw new Error('Post body must not me empty');
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -37,6 +41,10 @@ module.exports = {
       })
 
       const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      })
 
       return post;
     },
@@ -54,6 +62,33 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    async likePost(_, { postId }, context){
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if(post){
+        if(post.likes.find(like => like.username === username)){
+          //post already liked, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          //Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          })
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError('Post not found');
+      }
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 }
