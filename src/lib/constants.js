1e3c5a622a4d5eb4ede8f1508427df72c11642bd
Ezequiel Castellanos <51804994+EzequielCaste@users.noreply.github.com>
2022-11-14 17:07:32 -0300

    include Footer with links to repo

---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+export const MAIN_TITLE = 'Ezequiel Castellanos | Social Media App';
+export const GITHUB_REPOSITORY =
+  'https://github.com/EzequielCaste/React-GraphQL-Social-App';
+export const CMS_NAME = 'Ezequiel';
+export const BLOG_URL = 'http://ezecastellanos.com.ar';
+export const HOME_OG_IMAGE_URL = 'https://i.imgur.com/kXSv53f.png';
